41392ff29bf293a7443d117a539e59580d8a1041
DESKTOP-3QKRIRT\Igor <vish_ig3@mail.ru>
2022-06-14 21:51:06 +0700

    [develop-posts-CRUD] 1. Add saving and deleting files methods in FilesService 2. Add decorator for endpoints with uploading files

--- src/files/files.service.interface.ts
+++ src/files/files.service.interface.ts
@@ -1,4 +1,6 @@
 export interface IFilesService{
-
+    deleteFile(filename: string): Promise<void>
+    createFiles(files): Promise<string[]>
+    createFile(file): Promise<string>
 }
 export const IFilesService = Symbol("IFilesService")
